442cc9135cb0b70fbea35047623ad41edbce7843
github username <github email address>
2020-01-30 18:12:22 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -31,13 +31,18 @@ class Song
 
   def self.find_by_name(name)
     @@all.find { |song| song.name == name }
+    # BELOW ALSO WORKS (flatiron's sol'n)
+    # self.all.detect{|song| song.name == name} ALSO WORKS
   end
 
   def self.find_or_create_by_name(name)
-    if !find_by_name(name)
-      create_by_name(name)
-    else find_by_name(name)
-    end
+    self.find_by_name(name) || self.create_by_name(name)
+
+    # BELOW ALSO WORKS (my original sol'n)
+    # if !find_by_name(name)
+    #   create_by_name(name)
+    # else find_by_name(name)
+    # end
   end
 
   def self.alphabetical
@@ -49,6 +54,16 @@ class Song
     song.name = filename.split(" - ").last.chomp(".mp3")
     song.artist_name = filename.split(" - ").first
     song
+
+    # BELOW ALSO WORKS (flatiron's sol'n)
+    # parts = filename.split(" - ")
+    # artist_name = parts[0]
+    # song_name = parts[1].gsub(".mp3", "")
+
+    # song = self.create
+    # song.name = song_name
+    # song.artist_name = artist_name
+    # song
   end
 
   def self.create_from_filename(filename)
@@ -57,6 +72,16 @@ class Song
     song.artist_name = filename.split(" - ").first
     @@all << song
     song
+
+    # BELOW ALSO WORKS (flatiron's sol'n)
+    # parts = filename.split(" - ")
+    # artist_name = parts[0]
+    # song_name = parts[1].gsub(".mp3", "")
+
+    # song = self.create
+    # song.name = song_name
+    # song.artist_name = artist_name
+    # song
   end
 
   def self.destroy_all
